64665253d632dd52d82f9e0f22c853f199d12cf5
mutantsan <kvaqich@gmail.com>
2022-08-30 19:03:15 +0300

    change error status wording

--- ckanext/validation_schema_generator/constants.py
+++ ckanext/validation_schema_generator/constants.py
@@ -3,23 +3,21 @@ TASK_KEY = u"validation_schema_generator"
 TASK_STATE_PENDING = u"Pending"
 TASK_STATE_FINISHED = u"Finished"
 TASK_STATE_NOT_GENERATED = u"Not generated"
-TASK_STATE_ERROR = u"Error"
+TASK_STATE_ERROR = u"Failed"
 
 CF_JOB_TIMEOUT = u"ckanext.validation_schema_generator.job_timeout"
 CF_JOB_TIMEOUT_DF = 3600
-
 CF_PASS_AUTH = u"ckanext.validation_schema_generator.pass_api_key"
 CF_PASS_AUTH_DF = True
-
 CF_API_KEY = u"ckanext.validation_schema_generator.api_key"
 
-APPLY_FOR_OPTIONS = ("dataset", "resource")
+APPLY_FOR_OPTIONS = (u"dataset", u"resource")
 APPLY_FOR_DATASET = APPLY_FOR_OPTIONS[0]
 APPLY_FOR_RESOURCE = APPLY_FOR_OPTIONS[1]
+APPLY_FOR_FIELD = u"apply_for"
 
-RES_SCHEMA_FIELD = "schema"
-PKG_SCHEMA_FIELD = "default_data_schema"
-APPLY_FOR_FIELD = "apply_for"
+RES_SCHEMA_FIELD = u"schema"
+PKG_SCHEMA_FIELD = u"default_data_schema"
 
-EMPTY_SCHEMA = ""
-UNAPPLIED = ""
+EMPTY_SCHEMA = u""
+UNAPPLIED = u""
