f542ee37e3999f50e89ae66fcf8d6e814ed7fc74
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-09-26 11:39:23 +1000

    generate alternative validation function for CKAN 2.8
    - The 'dict_only' validator doesn't exist on 2.8

--- ckanext/validation_schema_generator/logic/schema.py
+++ ckanext/validation_schema_generator/logic/schema.py
@@ -1,4 +1,7 @@
+# encoding: utf-8
+
 from ckan.logic.schema import validator_args
+import ckan.plugins.toolkit as tk
 
 import ckanext.validation_schema_generator.constants as const
 
@@ -27,17 +30,36 @@ def vsg_apply_schema(not_missing, resource_id_exists, one_of, ignore_missing,
     }
 
 
-@validator_args
-def vsg_hook_schema(not_missing, resource_id_exists, one_of, ignore_missing,
-                    vsg_validate_schema, unicode_safe, dict_only,
-                    vsg_is_resource_supportable, vsg_generation_started):
+if tk.check_ckan_version('2.9'):
 
-    return {
-        "id": [
-            not_missing, resource_id_exists, vsg_is_resource_supportable,
-            vsg_generation_started
-        ],
-        "error": [not_missing, dict_only],
-        "status": [not_missing, one_of(const.VALID_STATES)],
-        "schema": [ignore_missing, unicode_safe, vsg_validate_schema]
-    }
+    @validator_args
+    def vsg_hook_schema(not_missing, resource_id_exists, one_of, ignore_missing,
+                        vsg_validate_schema, unicode_safe, dict_only,
+                        vsg_is_resource_supportable, vsg_generation_started):
+
+        return {
+            "id": [
+                not_missing, resource_id_exists, vsg_is_resource_supportable,
+                vsg_generation_started
+            ],
+            "error": [not_missing, dict_only],
+            "status": [not_missing, one_of(const.VALID_STATES)],
+            "schema": [ignore_missing, unicode_safe, vsg_validate_schema]
+        }
+
+else:
+
+    @validator_args
+    def vsg_hook_schema(not_missing, resource_id_exists, one_of, ignore_missing,
+                        vsg_validate_schema, unicode_safe,
+                        vsg_is_resource_supportable, vsg_generation_started):
+
+        return {
+            "id": [
+                not_missing, resource_id_exists, vsg_is_resource_supportable,
+                vsg_generation_started
+            ],
+            "error": [not_missing],
+            "status": [not_missing, one_of(const.VALID_STATES)],
+            "schema": [ignore_missing, unicode_safe, vsg_validate_schema]
+        }
