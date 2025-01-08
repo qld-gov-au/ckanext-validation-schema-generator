0c64340ccbc311e898eb0976418a03177ff47795
ThrawnCA <shell_layer-github@yahoo.com.au>
2025-01-08 11:31:29 +1000

    [QOLDEV-1016] update test dependencies and sync scenario testing with other repos

--- bin/build.sh
+++ bin/build.sh
@@ -21,9 +21,9 @@ CKAN_GIT_VERSION=$CKAN_VERSION
 CKAN_GIT_ORG=qld-gov-au
 
 if [ "$CKAN_VERSION" = "2.11" ]; then
-    CKAN_GIT_VERSION=ckan-2.11.0
+    CKAN_GIT_VERSION=ckan-2.11.1
 elif [ "$CKAN_VERSION" = "2.10" ]; then
-    CKAN_GIT_VERSION=ckan-2.10.5
+    CKAN_GIT_VERSION=ckan-2.10.5-qgov.4
 elif [ "$CKAN_VERSION" = "2.9" ]; then
     CKAN_GIT_VERSION=ckan-2.9.9-qgov.4
 fi
